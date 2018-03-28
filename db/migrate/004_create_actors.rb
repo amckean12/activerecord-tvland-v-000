5f133dc79530babbcee5062d7c93a2f41a875baf
Alex McKean <amckean@me.com>
2018-03-28 15:12:07 +0000

    automatically backed up by learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,9 +1,9 @@
-class CreateActors < ActiveRecord::Migration 
-  def change 
+class CreateActors < ActiveRecord::Migration
+  def change
     create_table :actors do |a|
-      a.string :first_name 
+      a.string :first_name
       a.string :last_name
     end 
-  end 
-  
-end 
+  end
+
+end
---
+++ db/migrate/005_create_characters.rb
@@ -0,0 +1,7 @@
+class CreateCharacters < ActiveRecord::Migration
+  def change
+    create_table :characters do |c|
+      c.string :name
+      c.integer :show_id
+  end
+end
